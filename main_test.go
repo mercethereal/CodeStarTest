d7d8045d7d57c65a444765b92af989b1d9cb6b0b
mercethereal <neal.whitney@gmail.com>
2019-11-20 11:40:01 -0700

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ func TestHandler(t *testing.T) {
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
-		Body: "Congratulations",
+		Body: "wHOO HOO BOYs",
 	}
 
 	response, err := Handler(request)
